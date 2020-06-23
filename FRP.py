f8702723841c22b6ddf13d3fa0b35995348a26a6
Murali N G <muralignanasekaran@users.noreply.github.com>
2020-06-23 19:43:39 +0530

    Update FRP.py

--- FRP.py
+++ FRP.py
@@ -24,17 +24,8 @@ class file_validation:
         self.reason=reason
 
     
-    def download_file(self,file_name):
-        ftp = FTP(self.src_server_name)
-        ftp.login(user='u180164016.forkit_ftp', passwd = 'forkit@123')
-        os.chdir(self.tgt_file_path)
-        ftp.cwd(src_file_path)
-        localfile = open(file_name, 'wb')
-        ftp.retrbinary('RETR ' + file_name, localfile.write, 1024)
-        ftp.quit()
-        localfile.close()
-        
-    def validation(self):
+            
+    def download_file(self):
         ftp = FTP(self.src_server_name)
         ftp.login(user='u180164016.forkit_ftp', passwd = 'forkit@123')
         os.chdir(self.tgt_file_path)
@@ -42,8 +33,10 @@ class file_validation:
         for file_name in ftp.nlst():
             if len(re.split('[\W_]',file_name)) == self.file_element_cnt:
                 if re.search(self.file_name_pattern,file_name):
-                    self.download_file(file_name)
-
+                    localfile = open(file_name, 'wb')
+                    ftp.retrbinary('RETR ' + file_name, localfile.write, 1024)
+                    localfile.close()
+        ftp.quit()
 
 ########################---------Connecting to Database-----########################
 conn = pyodbc.connect('Driver={SQL Server};'
@@ -62,7 +55,7 @@ for row in data:
     file_id,src_name,src_server_name,src_file_path,file_name_pattern,file_element_cnt,file_delimiter, file_extension,tgt_server_name,tgt_file_path,tgt_file_name,failure_notification_email, reason,sample_file_name = row
     table_row = file_validation(file_id,src_name,src_server_name,src_file_path,file_name_pattern,file_element_cnt,file_delimiter, file_extension,tgt_server_name,tgt_file_path,tgt_file_name,failure_notification_email, reason,sample_file_name)
 
-table_row.validation()
+table_row.download_file()
 
 cursor.close()
 conn.close()
