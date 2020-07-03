4032e2cd751c20a67d2b8962cc1e52b83d0c47ac
Murali N G <muralignanasekaran@users.noreply.github.com>
2020-07-03 16:25:28 +0530

    Update FRP.py

--- FRP.py
+++ FRP.py
@@ -14,7 +14,7 @@ import shutil
 import time
 import boto3
 from datetime import datetime
-#import hashlib
+import hashlib
 from mail import EmailProcessor
 
 
@@ -47,7 +47,8 @@ class file_validation:
         failure_notification_email,
         work_directory,
         transform_spec_json,
-        sample_file_name,
+        sample_file_name
+        
     ):
         self.file_id = file_id
         self.src_name = src_name
@@ -63,11 +64,43 @@ class file_validation:
         self.failure_notification_email = failure_notification_email
         self.work_directory = work_directory
         self.transform_spec_json = transform_spec_json
+        
+    def lt_files_entry(self,file_name,checksum,error_descr,file_size,file_row_count):
+        try:
+            sql = "insert into forkit.dbo.lt_files values(NEXT VALUE FOR file_batch_key,'"+file_name+"','"+checksum+"','"+self.src_name+"','"+error_descr+"',"+str(file_size)+","+str(file_row_count)+",CURRENT_TIMESTAMP)"
+            #sql = 'insert into lt_files values(NEXT VALUE FOR file_batch_key,'+file_name+','+checksum+','+self.src_name+','+str(file_size)+','+str(file_row_count)+',CURRENT_TIMESTAMP)'
+            #sql = 'select count(*) from etl_src'
+            #print(sql)
+        except Exception as e:
+            logging.error(e)
+        return sql
+                    
 
     def columns_validation(self, file_name):
         logging.info("columns_validation for file : "+file_name)
         os.chdir(self.work_directory)
-        dataframe1 = pd.read_excel(file_name)
+        if self.file_extension == 'xlsx':
+            dataframe1 = pd.read_excel(file_name)
+        elif self.file_extension == 'xlsx':
+            dataframe1 = pd.read_csv(file_name)
+        elif self.file_extension == 'json':
+            dataframe1 = pd.read_json(file_name)
+        else:
+            logging.error('Invalid file extension')
+        checksum = hashlib.md5(open(file_name,'rb').read()).hexdigest()
+        try:
+            cur = conn.cursor()
+            cur.execute("select count(*) from forkit.dbo.lt_files where file_checksum = '"+checksum+"'")
+            for row in cur:
+                if row[0] != 0:
+                    error_descr = 'File already processed'
+                else:
+                    error_descr = ''
+            #print(error_descr)
+        except Exception as e:
+            logging.error(e)
+        file_size = os.stat(file_name).st_size
+        file_row_count = len(dataframe1.index)
         columns_list = list(dataframe1.columns.values)
         # print(columns_list)
         with open(self.transform_spec_json) as json_file:
@@ -81,12 +114,14 @@ class file_validation:
             if len(columns_list) == len(source_columns_list):
                 if columns_list == source_columns_list:
                     logging.info("Columns are matching")
-                    return True
+                    sql = self.lt_files_entry(file_name,checksum,error_descr,file_size,file_row_count)
+                    return sql,error_descr
                 else:
                     logging.error("Columns are not matching")
-                    return False
+                    return False,'error'
 
     def download_file(self):
+        error_descr = ''
         isFileAvailable = 0
         ftp = FTP(self.src_server_name)
         ftp.login(user="u180164016.forkit_ftp", passwd="forkit@123")
@@ -97,18 +132,27 @@ class file_validation:
             if len(re.split(r"[\W_]", file_name)) == self.file_element_cnt:
                 if re.search(self.file_name_pattern, file_name):
                     isFileAvailable = 1
-                    #checksum = hashlib.md5(open(file_name,'rb').read()).hexdigest()
                     localfile = open(file_name, "wb")
                     ftp.retrbinary("RETR " + file_name, localfile.write, 1024)
                     localfile.close()
-                    if(self.columns_validation(file_name)):
-                        shutil.copy(file_name, self.tgt_file_path)
-                        os.remove(file_name)
-                        logging.info(
-                            file_name+": is placed in the target directory")
+                    sql,error_descr = self.columns_validation(file_name)
+                    if(sql):
+                        try:
+                            cursor1 = conn.cursor()
+                            cursor1.execute(sql)
+                            cursor1.close()
+                        except Exception as e:
+                            logging.error(e)
+                        if error_descr== '':
+                            shutil.copy(file_name, self.tgt_file_path)
+                        else:
+                            os.remove(file_name)
+                            logging.info(file_name+": already processed hence placed in the target directory")
+                            
                     else:
                         logging.error(
                             file_name+": is not placed in the target directory due to columns mismatch")
+            error_descr = ''
 
         if isFileAvailable == 0:
             logging.info("No files matched the defined global pattern")
@@ -122,6 +166,7 @@ class file_validation:
         s3 = boto3.client('s3', aws_access_key_id=CONFIG["email_aws_access_key_id"],aws_secret_access_key=SECRETS["email_aws_secret_access_key"])
         bucket = client.Bucket(CONFIG["bucket"])
         folder = self.src_file_path
+        error_descr = ''
         for cList in bucket.objects.filter(Prefix=folder):
             cFiles= cList.key
             pattern = self.src_file_path+self.file_name_pattern
@@ -132,17 +177,31 @@ class file_validation:
                         file_name = run_timestamp+self.tgt_file_name
                         os.chdir(self.work_directory)
                         s3.download_file(CONFIG["bucket"],cFiles,file_name)
-                        if(self.columns_validation(file_name)):
-                            shutil.copy(file_name, self.tgt_file_path)
-                            os.remove(file_name)
-                            logging.info(
-                                    file_name+": is placed in the target directory")
+                        #checksum = hashlib.md5(open(file_name,'rb').read()).hexdigest()
+                        sql,error_descr = self.columns_validation(file_name)
+                        if(sql):
+                            try:
+                                cursor2 = conn.cursor()
+                                cursor2.execute(sql)
+                                cursor2.close()
+                            except Exception as e:
+                                logging.error(e)
+                            if error_descr == '':
+                                shutil.copy(file_name, self.tgt_file_path)
+                                logging.info(file_name+": is placed in the target directory")
+                            else:
+                                os.remove(file_name)
+                                logging.info(file_name+": already processed hence placed in the target directory")
+                            
+                            
                         else:
                             logging.error(
                             file_name+": is not placed in the target directory due to columns mismatch")
                     except Exception as e:
                         logging.error(e)
-                    
+            error_descr = ''
+    
+   
 
 
 ########################---------Connecting to Database-----########################
@@ -156,6 +215,8 @@ try:
     logging.info("Connetion to the DB (Server=MURALI\SQLEXPRESS, Database=ForkIT) successfully ")
 except Exception:
     logging.error("Counln't connect to DB (Server=MURALI\SQLEXPRESS, Database=ForkIT)")
+    
+
 try:
 	cursor = conn.cursor()
 	cursor.execute("select * from forkit.dbo.etl_src where src_name ='FTP1' ")
@@ -200,7 +261,7 @@ try:
 	cursor.close()
 
 except Exception as e:
-	logging.ERROR('Error while downloading files from FTP: '+e)
+	logging.error(e)
     
 
 ############################### Downloading files from mail #######################################
@@ -208,7 +269,7 @@ except Exception as e:
 
 
 SECRETS = {
-    "email_aws_secret_access_key": '*********'
+    "email_aws_secret_access_key": '**************'
 }
 
 CONFIG = {
@@ -277,5 +338,6 @@ try:
 	cursor.close()
 
 except Exception as e:
-	logging.ERROR('Error while downloading files from FTP: '+e)
+	logging.error('Error while downloading files from FTP: '+e)
+conn.commit()
 conn.close()
